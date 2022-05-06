a6a6241babace542ac3dd25729548ee9d4563baa
Thiago Nobrega <thiagons@live.com>
2022-05-06 16:26:15 -0300

    feat: create table users

--- desafio1.sql
+++ desafio1.sql
@@ -6,4 +6,12 @@ CREATE TABLE plans(
   plan_id INT PRIMARY KEY AUTO_INCREMENT,
   plan_name VARCHAR(100) NOT NULL,
   plan_value DECIMAL(5,2) NOT NULL,
-)
+);
+
+CREATE TABLE users(
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  user_name VARCHAR(100) NOT NULL,
+  user_age INT NOT NULL,
+  plan_id INT NOT NULL,
+  FOREIGN KEY (plan_id) REFERENCES plans(plan_id)
+);
