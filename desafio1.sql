d45e40f6d9f7f3552600225055b2e362ada6a426
Thiago Nobrega <thiagons@live.com>
2022-05-06 18:09:51 -0300

    feat: insert values in albums table

--- desafio1.sql
+++ desafio1.sql
@@ -104,3 +104,16 @@ VALUES
   ('Freedie Shannon'),
   ('Tyler Isle'),
   ('Fog');
+
+INSERT INTO albuns (album_name, artist_id, release_year)
+VALUES
+  ('Envious', 1, 1990),
+  ('Exuberant', 1, 1993),
+  ('Hallowed Steam', 2, 1995),
+  ('Incandescent', 3, 1998),
+  ('Temporary Culture', 4, 2001),
+  ('Library of liberty', 4, 2003)
+  ('Chained Down', 5, 2007),
+  ('Cabinet of fools', 5, 2012),
+  ('No guarantees', 5, 2015),
+  ('Apparatus', 6, 2015);
